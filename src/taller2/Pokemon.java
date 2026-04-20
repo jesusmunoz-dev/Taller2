2f4636b99e7d455f3d5a1166fe684fc898e7d58e
jesusmunoz-dev <jesus.munoz@alumnos.ucn.cl>
2026-04-19 23:17:34 -0400

    Primer menu

--- src/taller2/Pokemon.java
+++ src/taller2/Pokemon.java
@@ -1,5 +1,16 @@
 package taller2;
 
+//Nombre: Jesús Muñoz Cortes, RUT: 22.327.129-4, Carrera: Ingeniería Civil en Computación e Informatica
+
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Pokemon {
+	
+	
 
 }
